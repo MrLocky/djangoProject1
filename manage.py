d694c0655e3d3b8ba4106a0ae6a2569cecb9b568
kurskiy danila alekseevich <danila04122003@yandex.ru>
2024-04-03 20:50:54 +0300

    i

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-def main():
+def mainnnnnnnnnnZ():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject1.settings')
     try:
